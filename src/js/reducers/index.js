a120139aca8ac99c45ca92291c260ac90928c3bd
rajeev884 <kumar.rajeev884@gmail.com>
2018-03-30 12:25:55 +0530

    firsto

---
+++ src/js/reducers/index.js
@@ -0,0 +1,33 @@
+// const initialState = {
+//     articles: []
+//   };
+//   const rootReducer = (state = initialState, action) => state;
+//   export default rootReducer;
+
+// import { ADD_ARTICLE } from "../constants/action-types";
+// const initialState = {
+//   articles: []
+// };
+// const rootReducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case ADD_ARTICLE:
+//       return { ...state, articles: state.articles.concat(action.payload) };
+//     default:
+//       return state;
+//   }
+// };
+// export default rootReducer;
+
+import { ADD_ARTICLE } from "../constants/action-types";
+const initialState = {
+  articles: []
+};
+const rootReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_ARTICLE:
+      return { ...state, articles: [...state.articles, action.payload] };
+    default:
+      return state;
+  }
+};
+export default rootReducer;
